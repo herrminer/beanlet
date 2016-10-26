4eb94ff689917531ac0d243e44c701eada7e4ed0
Joseph Miner <herrminer@gmail.com>
2016-10-26 13:13:40 -0500

    encoding passwords

--- beanlet-web/src/main/java/com/beanlet/web/SecurityConfiguration.java
+++ beanlet-web/src/main/java/com/beanlet/web/SecurityConfiguration.java
@@ -1,9 +1,12 @@
 package com.beanlet.web;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -26,4 +29,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         .logoutSuccessUrl("/login");
   }
 
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
 }
