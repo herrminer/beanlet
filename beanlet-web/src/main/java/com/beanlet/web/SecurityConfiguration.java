99d9d3eb3f11d4424da2306229850df5472f6deb
Joseph Miner <herrminer@gmail.com>
2016-10-27 10:24:23 -0500

    remember me works for two weeks

--- beanlet-web/src/main/java/com/beanlet/web/SecurityConfiguration.java
+++ beanlet-web/src/main/java/com/beanlet/web/SecurityConfiguration.java
@@ -1,16 +1,29 @@
 package com.beanlet.web;
 
+import org.joda.time.DateTime;
+import org.joda.time.Period;
+import org.joda.time.PeriodType;
+import org.joda.time.format.PeriodFormatter;
+import org.joda.time.format.PeriodFormatterBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.RememberMeServices;
+import org.springframework.security.web.authentication.rememberme.TokenBasedRememberMeServices;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+  @Autowired
+  private UserDetailsService userDetailsService;
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
@@ -26,7 +39,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         .and()
       .logout()
         .logoutUrl("/logout")
-        .logoutSuccessUrl("/login");
+        .logoutSuccessUrl("/login")
+        .deleteCookies("remember-me")
+        .and()
+      .rememberMe()
+        .userDetailsService(userDetailsService);
   }
 
   @Bean
