25f6769bc3d477334fdf0e29c7cf57d550726b5a
Joseph Miner <herrminer@gmail.com>
2016-11-03 12:52:32 -0500

    formatting DateTime consistently

--- beanlet-web/src/main/java/com/beanlet/web/controller/CountItController.java
+++ beanlet-web/src/main/java/com/beanlet/web/controller/CountItController.java
@@ -6,6 +6,8 @@ import com.beanlet.web.jpa.User;
 import com.beanlet.web.service.BeanletService;
 import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.convert.ConversionService;
+import org.springframework.format.Formatter;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,9 +20,13 @@ public class CountItController {
   @Autowired
   private BeanletService beanletService;
 
+  @Autowired
+  private ConversionService conversionService;
+
   @PostMapping("/countIt")
-  public Beanlet countIt(@RequestParam EntityId<Beanlet> beanletId, @AuthenticationPrincipal User user) {
-    return beanletService.countIt(user.getId(), beanletId);
+  public String countIt(@RequestParam EntityId<Beanlet> beanletId, @AuthenticationPrincipal User user) {
+    Beanlet beanlet = beanletService.countIt(user.getId(), beanletId);
+    return conversionService.convert(beanlet.getDateLastLogged(), String.class);
   }
 
 }
