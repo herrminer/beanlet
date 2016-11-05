4afc73730449dae7e06294cd39e23f1824e80f50
Joseph Miner <herrminer@gmail.com>
2016-11-04 21:10:47 -0500

    adding new beanlets

--- beanlet-web/src/main/java/com/beanlet/web/controller/BeanletsController.java
+++ beanlet-web/src/main/java/com/beanlet/web/controller/BeanletsController.java
@@ -1,5 +1,6 @@
 package com.beanlet.web.controller;
 
+import com.beanlet.web.jpa.Beanlet;
 import com.beanlet.web.jpa.User;
 import com.beanlet.web.service.BeanletService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,11 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class BeanletsController {
@@ -18,6 +24,14 @@ public class BeanletsController {
   public String displayBeans(Model model, @AuthenticationPrincipal User user) {
     model.addAttribute("beanlets", beanletService.getBeanletsForUserId(user.getId()));
     return "beanlets";
+  }
+
+  @PostMapping("/beanlets")
+  public String addBeanlet(Model model, @RequestParam String name, @AuthenticationPrincipal User user) {
+    List<Beanlet> beanlets = new ArrayList<>(1);
+    beanlets.add(beanletService.addBeanlet(user.getId(), name));
+    model.addAttribute("beanlets", beanlets);
+    return "beanlet-items";
   }
 
 }
