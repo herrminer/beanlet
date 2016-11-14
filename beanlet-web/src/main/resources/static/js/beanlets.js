1f6025f248d0a4cb4cf4e336cf0e5149c36455ca
Joseph Miner <herrminer@gmail.com>
2016-11-14 06:48:20 -0600

    adding beanlet, still need to spice it up a bit

--- beanlet-web/src/main/resources/static/js/beanlets.js
+++ beanlet-web/src/main/resources/static/js/beanlets.js
@@ -6,16 +6,21 @@ $(document).ajaxError(function(event, jqxhr, settings, thrownError) {
   }
 });
 
-// function to count a bean
 var service = {
   countIt: function(beanletId, responseHandler){
     if (typeof responseHandler != 'function') {
       throw new Error('successHandler not a function: ' + responseHandler);
     }
-    console.log('service.countIt ' + beanletId);
     var timeZone = jstz.determine().name();
     var csrfToken = $("meta[name='_csrf']").attr("content");
     $.post("/countIt", {beanletId:beanletId, _csrf:csrfToken, timeZone: timeZone})
+      .done(function(data, textStatus, jqXHR){
+        responseHandler(data);
+      });
+  },
+  addBeanlet: function(name, responseHandler){
+    var token = $("meta[name='_csrf']").attr("content");
+    $.post("/beanlets", {_csrf:token, name:name})
       .done(function(data, textStatus, jqXHR){
         responseHandler(data);
       });
@@ -23,57 +28,49 @@ var service = {
 };
 
 var beanlets = {
+  initializePage: function(){
+    beanlets.initializeBeanlets();
+    beanlets.initializeAddBeanletForm();
+  },
+  initializeAddBeanletForm: function(){
+    $('#modal-add').on('shown.bs.modal', function(){
+      $('#beanlet-name').focus()
+    });
+    $('#btn-add-beanlet').click(beanlets.addBeanlet);
+  },
+  initializeBeanlets: function(){
+    $('.new').each(function(){
+      var beanletId = $(this).attr('id');
+      $('#ci-'+beanletId).click(function(){beanlets.countIt(beanletId); return false;});
+    }).removeClass('new');
+  },
   countIt: function(beanletId) {
     $('#count-'+beanletId).text('').addClass('loading');
     service.countIt(beanletId, beanlets.countItResponseHandler);
-    // setTimeout(function(){
-    // }, 500);
   },
-  countItResponseHandler: function(countItResponse){
+  countItResponseHandler: function(countItResponse) {
     var beanletId = countItResponse.beanletId;
-    $('#ll-'+beanletId).text(countItResponse.lastLogged);
-    $('#count-'+beanletId).removeClass('loading').text(countItResponse.beanCount);
-    var li = $('#'+beanletId);
+    $('#ll-' + beanletId).text(countItResponse.lastLogged);
+    $('#count-' + beanletId).removeClass('loading').text(countItResponse.beanCount);
+    var li = $('#' + beanletId);
     li.addClass('bg-success', 250);
-    setTimeout(function(){ li.removeClass('bg-success', 500) }, 2000);
+    setTimeout(function () {
+      li.removeClass('bg-success', 500)
+    }, 2000);
+  },
+  addBeanlet: function(){
+    var name = $('#beanlet-name').val();
+    service.addBeanlet(name, beanlets.addBeanletResponseHandler);
+  },
+  addBeanletResponseHandler: function(response){
+    var newRows = $(response).find('li.beanlet');
+    newRows.prependTo($('#beanlets'));
+    beanlets.initializeBeanlets();
+    $('#modal-add').modal('hide');
   }
-};
-
-// function to initialize beanlets with appropriate event handlers
-var initializeBeanlets = function(){
-  $('.new').each(function(){
-    var beanletId = $(this).attr('id');
-    $('#ci-'+beanletId).click(function(){beanlets.countIt(beanletId); return false;});
-  }).removeClass('new');
 };
 
 // run when the DOM is ready
 $(function(){
-  initializeBeanlets();
-  $('#modal-add').on('shown.bs.modal', function () {
-    $('#beanlet-name').focus()
-  })
-  $('#btn-add-beanlet').on('click', function(){
-    console.log('adding beanlet');
-    $('#modal-add').modal('hide');
-  });
-/*
-  $('#btn-add-beanlet').click(function(){
-    var token = $("meta[name='_csrf']").attr("content");
-    var name = $('#beanlet-name').val();
-    $.post("/beanlets", {_csrf:token, name:name})
-      .done(function(data, textStatus, jqXHR){
-        var beanlets = $(data).find('div.beanlet');
-        beanlets.appendTo($('#beanlet-list'));
-        initializeBeanlets();
-      })
-      .fail(function(jqXHR, textStatus, errorThrown){
-        console.log(jqXHR.responseText);
-        // detect if the session has expired
-        if (jqXHR.status == 403) {
-          window.location = '/';
-        }
-      });
-  });
-*/
+  beanlets.initializePage();
 });
