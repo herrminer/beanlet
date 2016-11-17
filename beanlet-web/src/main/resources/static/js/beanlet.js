afb3810f1a41446ff9a747b8cdf44af41ef147d1
Joseph Miner <herrminer@gmail.com>
2016-11-16 19:51:59 -0600

    much simpler (better) table!

--- beanlet-web/src/main/resources/static/js/beanlet.js
+++ beanlet-web/src/main/resources/static/js/beanlet.js
@@ -3,9 +3,18 @@ var service = {
 };
 
 var beanlet = {
-
+  initializePage: function () {
+    for (var i=0; i < 31; i++) {
+      $('#d'+i).text(i+1);
+    }
+    $('#beans-table').find('td').each(function(i, it){
+      if (i % 3 == 0 && i < 31)
+        $(it).addClass('bg-success');
+    });
+  }
 };
 
 $(function () {
   console.log('initializing beanlet page');
+  beanlet.initializePage();
 });
