d2c496f4652348b3464b461708eb31b574eb7700
Joseph Miner <herrminer@gmail.com>
2016-11-17 12:58:21 -0600

    loading calendar with dates...woohoo!

--- beanlet-web/src/main/resources/static/js/beanlet.js
+++ beanlet-web/src/main/resources/static/js/beanlet.js
@@ -1,12 +1,37 @@
-var service = {
+// handle ajax errors globally
+$(document).ajaxError(function(event, jqxhr, settings, thrownError) {
+  console.log('ajax error: ' + thrownError);
+  if (jqxhr.status == 403) {
+    window.location = '/';
+  }
+});
 
+var service = {
+  getCalendar: function(year, month, timeZone, responseHandler){
+    $.get("/beanlets/12345/calendar", {timeZone:timeZone})
+      .done(function(data, textStatus, jqXHR){
+        responseHandler(data);
+      });
+  }
 };
 
 var beanlet = {
-  initializePage: function () {
-    for (var i=0; i < 31; i++) {
-      $('#d'+i).text(i+1);
+  initializeCalendar: function () {
+    var timeZone = jstz.determine().name();
+    service.getCalendar(null, null, timeZone, beanlet.getCalendarResponseHandler);
+  },
+  getCalendarResponseHandler: function(beanletCalendar) {
+    var days = beanletCalendar.days;
+    var day;
+    for (var i=0; i < days.length; i++) {
+      day = days[i];
+      $('#d'+i)
+        .text(day.dayOfMonth)
+        .attr('class', day.currentMonth ? '' : 'not-current');
     }
+  },
+  initializePage: function () {
+    beanlet.initializeCalendar();
     $('#beans-table').find('td').each(function(i, it){
       var jq = $(it);
       jq.click(function(){$(this).toggleClass('bg-success', 250)});
